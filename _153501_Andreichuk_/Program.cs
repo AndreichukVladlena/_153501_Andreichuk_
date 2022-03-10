7019977769d4ea74f35b88a43cf2998a5bd47ed9
AndreichukVladlena <andreychuk04@mail.ru>
2022-03-10 14:48:50 +0300

    Добавьте файлы проекта.

---
+++ _153501_Andreichuk_/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _153501_Andreichuk_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите делимое : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите делитель : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a / b;
+            Console.Write("\nЧастное : " + c);
+            Console.ReadKey();
+        }
+    }
+}
+
+
